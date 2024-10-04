1bd916bd42260eb3a05a072661dbefdbf6fba928
Ge6ben <dl22gesh40@gmail.com>
2024-10-04 18:38:53 +0300

    feat:update environments keys

--- supabase/functions/doctors/lib/supabase.ts
+++ supabase/functions/doctors/lib/supabase.ts
@@ -2,12 +2,11 @@ import {createClient, SupabaseClient} from "supabase";
 import {Database as DatabaseTypes} from "./types/database.types.ts";
 
 export type SupabaseClientType = SupabaseClient<DatabaseTypes, "public">;
-// TODO: Replace with your own key! get it from running `supabase status`
-const ANON_KEY = "";
+
 export function getSupabaseClient(authHeader: string) {
     return createClient<SupabaseClientType>(
-        "http://127.0.0.1:54321",
-        ANON_KEY,
+        Deno.env.get("SUPABASE_URL"),
+        Deno.env.get("SUPABASE_ANON_KEY"),
         {
             global: {
                 headers: {
